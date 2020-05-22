da580a36054bac96919c4f7e28cfb644fe8e3f14
Michael Causey <michaelhcausey@gmail.com>
2020-05-22 19:48:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -36,9 +36,12 @@ def get_japanese_emoticon(library, english_emoticon)
   # code goes here
   # input "english_emoticon", output = "japanese_emoticon"
   # emoticon_language = 2 hashes: 1 for english, 1 for japanese 
+  japanese_emoticon = " "
   library_hash = load_library(library)
   library_hash.each do |emoticon_name, emoticon_language|
     emoticon_language.each do |key, value|
+      if value = english_emoticon
+        japanese_emoticon = library_hash[emoticon_name][:japanese]
       # binding.pry
     end
   end
